182f34fceaeacbf359e8c8a2e65722f5dd796efa
haoyujin <haoyujin2012@gmail.com>
2017-05-07 19:00:44 +0800

    cope YY'S seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,59 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "這個種子檔會自動建立一個admin帳號, 並且創建 10 個 public jobs, 以及10個hidden jobs"
 
-**/---------------just convenient--------------
+create_account = User.create([email: 'example@gmail.com', password: '12345678', password_confirmation: '12345678', is_admin: 'true'])
+puts "Admin account created."
 
-User.create!(email: "test1@gmail.com", password: "aaaaaa", password_confirmation: "aaaaaa", is_admin: true)
+create_jobs = for i in 1..10 do
+  Job.create!([title: "Job no.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false",work_place:"上海"])
+end
+puts "10 Public jobs created."
 
-Job.create!(title:蜘蛛人 , description:"高空作业，五险一金 ", wage_upper_bound: 4000 , wage_lower_bound: 6000 , is_hidden: false, work_place: "北京")
-
-Job.create!(title:油漆工 , description:"带培训，管吃管住 ", wage_upper_bound: 4000 , wage_lower_bound: 6000 , is_hidden: true, work_place: "北京")
-
-Job.create!(title:木工 , description:"高空作业，五险一金 ", wage_upper_bound: 4000 , wage_lower_bound: 6000 , is_hidden: false, work_place: "深圳")
-
-Job.create!(title:石匠 , description:"高空作业，五险一金 ", wage_upper_bound: 4000 , wage_lower_bound: 6000 , is_hidden: false, work_place: "上海")
-
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
-Job.create!()
+create_jobs = for i in 1..10 do
+  Job.create!([title: "Job no.#{i+10}", description: "這是用種子建立的第 #{i+10} 個Hidden工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "true",work_place:"北京"])
+end
+puts "10 Hidden jobs created."
